a1b181b64ef28e76f20d03e91257c70383c5e839
Your mato <you@example.com>
2020-12-20 18:04:16 +0800

    Initial Commit

---
+++ SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by kittawat phuangsombat on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
